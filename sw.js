3b14de48090fc683822fb2bfa314a152fc9f0b62
gorf-technology <limbyeongchang110@gmail.com>
2026-06-25 16:02:49 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = 'photo-drop-v1';
+const FILES = [
+  '/photo-drop-game/',
+  '/photo-drop-game/index.html',
+  '/photo-drop-game/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
